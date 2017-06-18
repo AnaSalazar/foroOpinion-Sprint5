b15db073730698fb0dbc73318c05fac4c4523028
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-06-18 15:24:53 -0500

    Solucionado problema de visualización debido al http/https

--- js/verTopic.js
+++ js/verTopic.js
@@ -1,8 +1,8 @@
 var topicId = getParameterByName('topic_id');
 
 var api = {
-  url: "http://examen-laboratoria-sprint-5.herokuapp.com/topics/" + topicId,
-  urlResponses: "http://examen-laboratoria-sprint-5.herokuapp.com/topics/" + topicId + "/responses"
+  url: "https://examen-laboratoria-sprint-5.herokuapp.com/topics/" + topicId,
+  urlResponses: "https://examen-laboratoria-sprint-5.herokuapp.com/topics/" + topicId + "/responses"
 }
 
 var funcionInicial = function () {
